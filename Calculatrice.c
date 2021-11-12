19b0437b85876c9e944a3f8c991ec18edf26bc2f
Mehdi <lagdimi.mehdi@gmail.com>
2021-11-12 12:01:16 +0100

    Code compiles but does not work as expected

--- Calculatrice.c
+++ Calculatrice.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 //#include <math.h>
 #include <string.h>
 
-#define SIZE = 20;
+#define SIZE 20
 // arithmetic functions
 float arithmFunc(float x, float y, char oper){
     if (oper == '+') return x + y;
@@ -16,27 +17,34 @@ float arithmFunc(float x, float y, char oper){
 //    printf("Welcome to lightCalc!\n Please, type the arithmetic operation you want (please write it in this format XoperationY\n ex: 5+7\n");
 //    scanf(" %s", &operStr);
 //}
-void searchOperand(char operStr[]){
+float searchOperator(char operStr[]){
     //should I search the operand as a char or as an int?? check strch()
-    char operands[4] = {'+', '-', '*', '/'};
+    char operators[4] = {'+', '-', '*', '/'};
     char* ch;
+    double vars[2];
     for (int i = 0; i < 4;i++){  //4 possible operands in this program
-        ch = strchar(operStr, operands[i]);
+        ch = strchr(operStr, operators[i]);
         if(ch == NULL) { continue;}
-        else {
-            arithmFunc(ch[0]);
-            break; //is this necessary though??
+        short j = 0;  /* this is to append vars array with input operands; It is possible to process more than 2 operands in this case
+        though, we have to make the value 2 as a variable or another algorithm */
+        while (ch != NULL && j < 2) {
+            //perfom string splitting
+            char* splitStr = strtok(operStr, ch);
+            //convert string to float/double to pass it as argument to arithmFunc
+            vars[j] = strtod(splitStr, NULL);
+            j++;
         }
-    }
+        }
+        return (arithmFunc(vars[0], vars[1] ,ch[0]));
 }
 
 void main(){
 //    input();
-
+    float result;
     char operStr[SIZE];
     printf("Welcome to lightCalc!\n Please, type the arithmetic operation you want (please write it in this format XoperationY\n ex: 5+7\n");
     scanf(" %s", &operStr);
-    searchOperand(operStr);
-//    showResults();
+    result = searchOperator(operStr);
+    printf("\n Result of operation : %f", result);
 
 }
